6ab63dc1b264b7277a0c7bdea5f3a955c0642516
ilgianfra <gianfranco.garipoli@gmail.com>
2015-11-24 08:41:01 +0100

    setup

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var sirAngular = angular.module('sirAngular', ['ngNewRouter'])
+var sirAngular = angular.module('sirAngular', ['ngNewRouter', 'firebase'])
                         .controller('AppController', ['$router', AppController]);
 
 function AppController($router) {
@@ -12,7 +12,7 @@ function AppController($router) {
 
 var editorSt;
 
-sirAngular.directive('sirTrevor', function() {
+sirAngular.directive('sirTrevor', function(SirService) {
   return {
     require: '?ngModel',
     link: function(scope, elm, attr, ngModel) {
@@ -24,6 +24,7 @@ sirAngular.directive('sirTrevor', function() {
           el: $(elm),
           blockTypes: ["Text", "Video"]
         });
+        // SirService.setInstance(editorSt);
       }
 
       ngModel.$render = function(value) {
@@ -42,10 +43,37 @@ sirAngular.directive('sirTrevor', function() {
   };
 });
 
-sirAngular.controller('HomeController', function() {
+sirAngular.service('SirService', function(){
+
+  this.text = '{"data":[{"type":"text","data":{"text":"Hello, Im **Sir Trevor**.Create some new blocks and see _what I can do_."}}]}';
 });
 
-sirAngular.controller('EditorController', function() {
+sirAngular.controller('HomeController', function($firebaseObject, SirService) {
+  var vm = this;
+  var ref = new Firebase("https://sirtrevor.firebaseio.com/home");
+
+  // vm.text = '{"data":[{"type":"text","data":{"text":"Hello, Im **Sir Trevor**.Create some new blocks and see _what I can do_."}}]}';
+  // console.log(SirService.getInstance());
+  
+  
+  vm.text =  $firebaseObject(ref);
+  // vm.text = '{"data":[{"type":"text","data":{"text":"Hello, Im **Sir Trevor**.Create some new blocks and see _what I can do_."}}]}';
+  vm.text.$add('{"data":[{"type":"text","data":{"text":"Hello, Im **Sir Trevor**.Create some new blocks and see _what I can do_."}}]}');
+  console.log(vm.text);
+
+  vm.send = function(){
+    // var editorSt = SirService.getInstance();
+    console.log('SEND', vm.text);
+    console.log(editorSt);
+    editorSt.onFormSubmit();
+    var data = editorSt.store.retrieve();
+    console.log(JSON.stringify(data));
+    // vm.text.$add(data);
+  };
+
+});
+
+sirAngular.controller('EditorController', function(SirService) {
   
   this.setBody = function() {
     this.body = '{"data":[{"type":"text","data":{"text":"Hello, Im **Sir Trevor**.Create some new blocks and see _what I can do_."}},{"type":"video","data":{"source":"youtube","remote_id":"hcFLFpmc4Pg"}}]}'; 
@@ -54,6 +82,6 @@ sirAngular.controller('EditorController', function() {
   this.clearBody = function() {
     this.body = '';
   }
-
+  console.log(editorSt);
   this.setBody();
 });
